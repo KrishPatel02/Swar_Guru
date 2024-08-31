f6ed0af5e91fb8e1a37469d1affe12ac49da8578
Cyph3rZ3r0 <nilax.pln@gmail.com>
2024-08-31 11:44:54 +0530

    Add Aboutus and ContactUs Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css';
+// import AboutAndContact from './Pages/AboutUs';
 
+// function App() {
+
+//   return (
+//     <>
+//       <AboutAndContact/>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+// import { ChakraProvider } from '@chakra-ui/react';
+// import AboutAndContact from './AboutAndContact';
+
+// function App() {
+//   return (
+//     <ChakraProvider>
+//       <AboutAndContact />
+//     </ChakraProvider>
+//   );
+// }
+
+// export default App;
+
+
+// src/App.jsx
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import AboutAndContact from './Pages/AboutUs';
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <ChakraProvider>
+      <AboutAndContact />
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
